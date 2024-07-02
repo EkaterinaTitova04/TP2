4b3e7d5708b1a0ce9c6f3f42decd338f11fe0960
EkaterinaTitova04 <2136224@cegepmontpetit.ca>
2024-07-02 19:04:39 -0400

    FCT Création Commutateur de langue i18n et configuration i18n
    Création de la PartialView pour la sélection de la langue et l’enregistrement du Cookie dans le HomeController et configuration du i18n

--- Départ/Controllers/HomeController.cs
+++ Départ/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using JuliePro.Models;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
 using System.Diagnostics;
 
 namespace JuliePro.Controllers
@@ -7,11 +9,14 @@ namespace JuliePro.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IStringLocalizer<HomeController> _localizer;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IStringLocalizer<HomeController> localizer)
         {
             _logger = logger;
+            _localizer = localizer;
         }
+
 
         public IActionResult Index()
         {
@@ -22,7 +27,22 @@ namespace JuliePro.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult SetLanguage(string culture, string returnUrl)
+        {
+            var cookie = CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture));
+            var name = CookieRequestCultureProvider.DefaultCookieName;
 
-        
+            Response.Cookies.Append(name, cookie, new CookieOptions
+            {
+                Path = "/",
+                Expires = DateTimeOffset.UtcNow.AddYears(1),
+            });
+
+            return LocalRedirect(returnUrl);
+        }
+
+
+
     }
 }
--- Départ/Program.cs
+++ Départ/Program.cs
@@ -1,16 +1,49 @@
 using JuliePro.Data;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System.Globalization;
 var builder = WebApplication.CreateBuilder(args);
+
+
+// Injecter la localisation ICI
+#region Localizer configuration
+CultureInfo[] supportedCultures = new[]
+{
+    new CultureInfo("en-US"),
+    new CultureInfo("fr-CA")
+};
+#endregion
+
 builder.Services.AddDbContext<JulieProDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("JulieProContext") ?? throw new InvalidOperationException("Connection string 'JulieProContext' not found."));
 });
 
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+                .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+                .AddDataAnnotationsLocalization();
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    options.DefaultRequestCulture = new RequestCulture(culture: "en-US", uiCulture: "en-US");
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+});
+builder.Services.AddDbContext<JulieProDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")).UseLazyLoadingProxies());
+
 
 var app = builder.Build();
+
+var locOptions = app.Services.GetService<IOptions<RequestLocalizationOptions>>();
+app.UseRequestLocalization(locOptions.Value);
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
